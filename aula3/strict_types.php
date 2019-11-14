f338a2cffb4a5150125fa5fdce90bfc1ba9308bf
Cecilia Ribeiro <cecilia.rb@gmail.com>
2019-11-13 23:43:08 -0300

    Create strict_types.php

---
+++ aula3/strict_types.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+function mostraTitulo(string $texto){
+  if($texto == 1){
+	  echo "valor correto";
+  }	else{
+	  echo "valor errado";
+  }
+}
+
+mostraTitulo(1);
+
+
+?>
